6afe9738bf33a1c06eb4008dae1b13062d578da1
MrBElga <danieleliasrumin@gmail.com>
2025-02-19 17:26:44 +0000

    Sync automático do repo do Paulo

---
+++ Primitivas-Graficas/ProcessamentoImagens/frmPrincipal.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ProcessamentoImagens
+{
+    public partial class frmPrincipal : Form
+    {
+        private Image image;
+        private Bitmap imageBitmap;
+        private HSI[,] hsi;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrirImagem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = "";
+            openFileDialog.Filter = "Arquivos de Imagem (*.jpg;*.gif;*.bmp;*.png)|*.jpg;*.gif;*.bmp;*.png";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                image = Image.FromFile(openFileDialog.FileName);
+                pictBoxImg.Image = image;
+                pictBoxImg.SizeMode = PictureBoxSizeMode.Normal;
+                imageBitmap = (Bitmap)image;
+                hsi = rgbToHsi();
+            }
+        }
+
+        private HSI[,] rgbToHsi()
+        {
+            int height = image.Height;
+            int width = image.Width;
+            HSI[,] hsi = new HSI[width, height];
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++)
+                {
+                    int r = imageBitmap.GetPixel(x, y).R;
+                    int g = imageBitmap.GetPixel(x, y).G;
+                    int b = imageBitmap.GetPixel(x, y).B;
+
+                    float normR = (float)r / (r + g + b);
+                    float normG = (float)g / (r + g + b);
+                    float normB = (float)b / (r + g + b);
+
+                    float h = (float)Math.Acos((0.5 * ((r - g) + (r - b))) / Math.Sqrt((r - g) * (r - g) + (r - b) * (g - b)));
+                    if(b>g)
+                    {
+                        h = (float)(2 * Math.PI - h);
+                    }
+
+                    float s = 1 - 3 * Math.Min(Math.Min(normR, normG), normB);
+
+                    float i = (float)(r + g + b) / (3*255);
+
+                    int H = (int)(h * 180 / Math.PI);
+                    int S = (int)(s * 100);
+                    int I = (int)(i * 255);
+
+                    hsi[x, y] = new HSI(H, S, I);
+                }
+            }
+            return hsi;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            pictBoxImg.Image = null;
+        }
+
+        private void btnLuminanciaComDMA_Click(object sender, EventArgs e)
+        {
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            Filtros.convert_to_grayDMA(imageBitmap, imgDest);
+            pictBoxImg.Image = imgDest;
+        }
+
+        private void btnNegativoComDMA_Click(object sender, EventArgs e)
+        {
+            Bitmap imgDest = new Bitmap(image);
+            imageBitmap = (Bitmap)image;
+            Filtros.negativoDMA(imageBitmap, imgDest);
+            pictBoxImg.Image = imgDest;
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized; // Define a janela como maximizada
+        }
+
+
+    }
+}
---
+++ Primitivas-Graficas/ProcessamentoImagens/frmPrincipal.designer.cs
@@ -0,0 +1,121 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProcessamentoImagens
+{
+    partial class frmPrincipal
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.menuSuperiorPanel = new System.Windows.Forms.Panel();
+            this.btnAbrirImagem = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.pictureBoxPanel = new System.Windows.Forms.Panel();
+            this.pictBoxImg = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuLateralPanel = new System.Windows.Forms.Panel();
+            this.menuSuperiorPanel.SuspendLayout();
+            this.pictureBoxPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuSuperiorPanel
+            // 
+            this.menuSuperiorPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.menuSuperiorPanel.Controls.Add(this.btnAbrirImagem);
+            this.menuSuperiorPanel.Controls.Add(this.btnLimpar);
+            this.menuSuperiorPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.menuSuperiorPanel.Location = new System.Drawing.Point(0, 0);
+            this.menuSuperiorPanel.Name = "menuSuperiorPanel";
+            this.menuSuperiorPanel.Size = new System.Drawing.Size(1326, 60);
+            this.menuSuperiorPanel.TabIndex = 2;
+            // 
+            // btnAbrirImagem
+            // 
+            this.btnAbrirImagem.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnAbrirImagem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbrirImagem.ForeColor = System.Drawing.Color.White;
+            this.btnAbrirImagem.Location = new System.Drawing.Point(10, 10);
+            this.btnAbrirImagem.Name = "btnAbrirImagem";
+            this.btnAbrirImagem.Size = new System.Drawing.Size(120, 40);
+            this.btnAbrirImagem.TabIndex = 106;
+            this.btnAbrirImagem.Text = "Abrir Imagem";
+            this.btnAbrirImagem.UseVisualStyleBackColor = false;
+            this.btnAbrirImagem.Click += new System.EventHandler(this.btnAbrirImagem_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(140, 10);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(120, 40);
+            this.btnLimpar.TabIndex = 107;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // pictureBoxPanel
+            // 
+            this.pictureBoxPanel.BackColor = System.Drawing.Color.White;
+            this.pictureBoxPanel.Controls.Add(this.pictBoxImg);
+            this.pictureBoxPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxPanel.Location = new System.Drawing.Point(0, 60);
+            this.pictureBoxPanel.Name = "pictureBoxPanel";
+            this.pictureBoxPanel.Size = new System.Drawing.Size(1106, 694);
+            this.pictureBoxPanel.TabIndex = 0;
+
+            // 
+            // menuLateralPanel
+            // 
+            this.menuLateralPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.menuLateralPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.menuLateralPanel.Location = new System.Drawing.Point(1106, 60);
+            this.menuLateralPanel.Name = "menuLateralPanel";
+            this.menuLateralPanel.Size = new System.Drawing.Size(220, 694);
+            this.menuLateralPanel.TabIndex = 1;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 754);
+            this.Controls.Add(this.pictureBoxPanel);
+            this.Controls.Add(this.menuLateralPanel);
+            this.Controls.Add(this.menuSuperiorPanel);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Processamento de Imagens";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.menuSuperiorPanel.ResumeLayout(false);
+            this.pictureBoxPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel menuSuperiorPanel;
+        private System.Windows.Forms.Button btnAbrirImagem;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Panel pictureBoxPanel;
+        private System.Windows.Forms.PictureBox pictBoxImg;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private Panel menuLateralPanel;
+    }
+}
